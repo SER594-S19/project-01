c9594dbc043758978adf9bb23b6c5add4bed6168
Sumanth Paranjape <sparanj2@asu.edu>
2019-02-03 20:24:28 -0700

    Adding Face detection parameters with getters/setters in Data file

--- Core/Data.java
+++ Core/Data.java
@@ -11,10 +11,82 @@ public class Data {
 
     private double time;
     private double value;
+    private double agreement;
+    private double concentrating;
+    private double disagreement;
+    private double interested;
+    private double thinking;
+	private double unsure;
+    
+    public double getAgreement() {
+		return agreement;
+	}
 
-    public Data(double time, double value) {
+	public void setAgreement(double agreement) {
+		this.agreement = agreement;
+	}
+
+	public double getConcentrating() {
+		return concentrating;
+	}
+
+	public void setConcentrating(double concentrating) {
+		this.concentrating = concentrating;
+	}
+
+	public double getDisagreement() {
+		return disagreement;
+	}
+
+	public void setDisagreement(double disagreement) {
+		this.disagreement = disagreement;
+	}
+
+	public double getInterested() {
+		return interested;
+	}
+
+	public void setInterested(double interested) {
+		this.interested = interested;
+	}
+
+    public double getThinking() {
+		return thinking;
+	}
+
+	public void setThinking(double thinking) {
+		this.thinking = thinking;
+	}
+
+
+    public double getUnsure() {
+		return unsure;
+	}
+
+	public void setUnsure(double unsure) {
+		this.unsure = unsure;
+	}
+
+	public Data(double time, double value) {
         this.time = time;
         this.value = value;
+    }
+	
+	public Data(double time, double val1, double val2 , double val3, double val4, double val5, double val6) {
+        this.time = time;
+        /*
+         * private double agreement;
+    private double concentrating;
+    private double disagreement;
+    private double interested;
+    private double thinking;
+	private double unsure;*/
+        this.agreement = val1;
+        this.concentrating = val2;
+        this.disagreement = val3;
+        this.interested = val4;
+        this.thinking = val5;
+        this.unsure = val6;
     }
 
     public void setData(double data) {
