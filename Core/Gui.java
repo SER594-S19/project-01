586e5c1d0eb0190b423b80acccd55f22eaff147e
srivanreddy <sgutha3@asu.edu>
2019-02-03 19:27:23 -0700

    Added Radio buttons and related GIF's to the GUI

--- Core/Gui.java
+++ Core/Gui.java
@@ -7,6 +7,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,26 +15,67 @@ import javax.swing.JPanel;
 
 public class Gui extends JPanel implements ActionListener {
 
-  private static Model model;
+	private static final long serialVersionUID = 1L;
 
-  private final int PORT = 1594;
+private static Model model;
+  
+  static String agreementString = "Agreement";
+  static String concentratingString = "Concentrating";
+  static String disagreementString = "Disagreement";
+  private final int PORT = 5914;
   protected JLabel labelPublishPort;
-  private final JButton buttonConnect = new JButton("run");
+  private final JButton buttonConnect = new JButton("RUN");
+  
+  JLabel moodGif;
 
-  private Component createPanelSouth() {
+  private Component createBottomPanel() {
 
-    JPanel labels = new JPanel();
-    labels.setBackground(Color.GRAY);
-    labels.add(new JLabel("  Publishing at port: "));
+    JPanel portNumber = new JPanel();
+    portNumber.setBackground(Color.RED);
+    portNumber.add(new JLabel("  Publishing at port: "));
     labelPublishPort = new JLabel("" + PORT);
-    labels.add(labelPublishPort);
-    JPanel panel = new JPanel(new BorderLayout());
-    panel.setBackground(Color.CYAN);
-    panel.add(labels, BorderLayout.WEST);
-    panel.add(buttonConnect, BorderLayout.EAST);
+    portNumber.add(labelPublishPort);
+    
+    JRadioButton agreementButton = new JRadioButton(agreementString);
+    agreementButton.setActionCommand(agreementString);
+    agreementButton.setSelected(true);
+
+    JRadioButton concentratingButton = new JRadioButton(concentratingString);
+    concentratingButton.setActionCommand(concentratingString);
+    
+    JRadioButton disagreementButton = new JRadioButton(disagreementString);
+    disagreementButton.setActionCommand(disagreementString);
+    
+    ButtonGroup selectMood = new ButtonGroup();
+    selectMood.add(agreementButton);
+    selectMood.add(concentratingButton);
+    selectMood.add(disagreementButton);
+    
+    agreementButton.addActionListener(this);
+    concentratingButton.addActionListener(this);
+    disagreementButton.addActionListener(this);
+    
+    moodGif = new JLabel(createImageIcon(agreementString
+            + ".gif"));
+    
+    moodGif.setPreferredSize(new Dimension(177, 122));
+    
+    JPanel moodPanel = new JPanel(new GridLayout(0, 1));
+    moodPanel.add(agreementButton);
+    moodPanel.add(concentratingButton);
+    moodPanel.add(disagreementButton);
+    
+    add(moodPanel, BorderLayout.LINE_START);
+    add(moodGif, BorderLayout.CENTER);
+    setBorder(BorderFactory.createEmptyBorder(15,15,15,15));
+    
+    JPanel bottomPanel = new JPanel(new BorderLayout());
+    bottomPanel.add(portNumber, BorderLayout.WEST);
+    bottomPanel.add(buttonConnect, BorderLayout.EAST);
     buttonConnect.addActionListener(this);
     buttonConnect.setEnabled(true);
-    return panel;
+    return bottomPanel;  
+    
   }
 
   public Gui() {
@@ -41,7 +83,7 @@ public class Gui extends JPanel implements ActionListener {
     model = new Model(new DataGenerator(), new Publisher(PORT));
     this.setBackground(Color.WHITE);
     this.setLayout(new BorderLayout());
-    this.add(createPanelSouth(), BorderLayout.SOUTH);
+    this.add(createBottomPanel(), BorderLayout.SOUTH);
     Dimension screen = getToolkit().getScreenSize();
     this.setSize(screen.width / 2, 3 * screen.height / 4);
     this.setLocation((screen.width - getSize().width) / 2, (screen.height - getSize().height) / 2);
@@ -49,7 +91,10 @@ public class Gui extends JPanel implements ActionListener {
   }
 
   @Override
-  public void actionPerformed(ActionEvent e) {
+  public void actionPerformed(ActionEvent e) { 
+	  moodGif.setIcon(createImageIcon(e.getActionCommand()
+              + ".gif"));
+	  
     System.out.println("listener trigger");
     if (e.getSource() == buttonConnect) {
       if (buttonConnect.getText().compareTo("run") == 0) {
@@ -64,9 +109,20 @@ public class Gui extends JPanel implements ActionListener {
     }
   }
   
+  protected static ImageIcon createImageIcon(String path) {
+      java.net.URL gifURL = Gui.class.getResource(path);
+      if (gifURL != null) {
+          return new ImageIcon(gifURL);
+      } else {
+          System.err.println("Cannot find : " + path);
+          return null;
+      }
+  }
+  
+  
   public static void main(String[] args) {
 
-    JFrame frame = new JFrame("Simulator");
+    JFrame frame = new JFrame("Face Detection Simulator");
     frame.setLayout(new GridLayout(1, 1));
     frame.add(new Gui());
     frame.addWindowListener(new java.awt.event.WindowAdapter() {
@@ -79,6 +135,8 @@ public class Gui extends JPanel implements ActionListener {
     frame.pack();
     frame.setSize(500, 300);
     frame.setVisible(true);
+    
+   
   }
   
 }
