f981589981f4907333f09f1e5f19628b2f9211d2
Raam Prashanth  Namakkal Sudhakar <rnamakka@asu.edu>
2019-02-06 11:56:31 -0700

    Update ClientDemo.java

--- Client/ClientDemo.java
+++ Client/ClientDemo.java
@@ -8,24 +8,33 @@ import java.util.Observer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.text.StyledDocument;
 
 public class ClientDemo extends JFrame implements Observer, ActionListener {
 
+  private static final Logger LOGGER = Logger.getLogger(ClientDemo.class.getName());
   private final Subscriber subscriber;
   private final ExecutorService service;
-  private JTextArea textArea = new JTextArea();
+  private JTextPane textArea = new JTextPane();
   private JButton buttonConnect = new JButton("connect");
-  
+  StyledDocument doc = textArea.getStyledDocument();
+
+  JScrollPane scrollPane = new JScrollPane(textArea);
+
   public ClientDemo() {
 
     service = Executors.newCachedThreadPool();
     subscriber = new Subscriber("localhost", 1594);
    
     setLayout(new BorderLayout());
-    add(textArea, BorderLayout.CENTER);  
+    add(scrollPane, BorderLayout.CENTER);  
     add(buttonConnect, BorderLayout.SOUTH);  
     buttonConnect.addActionListener(this);
     addWindowListener(new java.awt.event.WindowAdapter() {
@@ -60,9 +69,14 @@ public class ClientDemo extends JFrame implements Observer, ActionListener {
   @Override
   public void update(Observable o, Object arg) {
     String data = ((Subscriber) o).getObject().toString();
-    if (data.compareTo("FIN") != 0)
-      textArea.append(data + "\n" );
-    else {
+    if (data.compareTo("FIN") != 0) {
+    	try
+    	{
+    	    doc.insertString(doc.getLength(), data + "\n", null);
+    	} catch(Exception e) { 
+    		LOGGER.log(Level.SEVERE, "Exception while writing in client", e);
+    	}
+    } else {
       close();
       buttonConnect.setEnabled(true);
     }    
