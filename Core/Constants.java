74cbd22fd880b9b2d8f8979e42435139745aa173
Sumanth Paranjape <sparanj2@asu.edu>
2019-02-06 15:51:24 -0700

    Making some changes to Constants file

--- Core/Constants.java
+++ Core/Constants.java
@@ -2,16 +2,6 @@ package Core;
 
 public class Constants {
 	
-	public static final int RANGEAGREEMENT0 = 3;
-	public static final int RANGEAGREEMENT1 = 9;
-	public static final int RANGECONCENTRATING0 = 0;
-	public static final int RANGECONCENTRATING1 = 0;
-	public static final int RANGEDISAGREEMENT0 = 3;
-	public static final int RANGEDISAGREEMENT1 = 4;
-	public static final int RANGEINTERESTED0 = 0;
-	public static final int RANGEINTERESTED1 = 0;
-	public static final int RANGETHINKING0 = 0;
-	public static final int RANGETHINKING1 = 4;
-	public static final int RANGEUNSURE0 = 2;
-	public static final int RANGEUNSURE1 = 13;
+	public static final int RANGE_HIGH = 10;
+	public static final int RANGE_LOW = 2;
 }
