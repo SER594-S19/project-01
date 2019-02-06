7952a98f16d34fd439769e1c09d1c9520c1adc50
schaud28 <schaud28@asu.edu>
2019-02-05 22:46:48 -0700

    Updated DataGenerator to generate Data

--- Core/DataGenerator.java
+++ Core/DataGenerator.java
@@ -1,12 +1,29 @@
 package Core;
 
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Observable;
 
-public class DataGenerator extends Observable implements Runnable {
+class DataGenerator extends Observable implements Runnable {
 
   private Data data;
   private boolean stop = false;
+
+
+
+  protected static HashMap<String, Double> faceParameters = new HashMap<>()
+  {
+    {
+      put("agreement", (double) 0);
+      put("disagreement", (double) 0);
+      put("frustrate", (double) 0);
+      put("concentrate", (double) 0);
+      put("thinking", (double) 0);
+      put("unsure", (double) 0);
+
+    }
+  };
+
 
   public void stop() {
     this.stop = true;
@@ -19,30 +36,41 @@ public class DataGenerator extends Observable implements Runnable {
   @Override
   public void run() {
     stop = false;
+
     Calendar calendar = Calendar.getInstance();
-    calendar.set(Calendar.HOUR_OF_DAY, 0);
-    calendar.set(Calendar.MINUTE, 0);
-    calendar.set(Calendar.SECOND, 0);
     long initialTime = calendar.getTimeInMillis();
-    double timeStamp = 0;
+    double timeStamp;
+
 
     while (!stop) {
-              System.out.println("data generator running");
+      System.out.println("data generator running");
+      float agreeInput = (float) (faceParameters.get("agreement")/10);
+      float disagreeInput = (float) (faceParameters.get("disagreement")/10);
+      float frusInput = (float) (faceParameters.get("frustrate")/10);
+      float concenInput = (float) (faceParameters.get("concentrate")/10);
+      float thinkInput = (float) (faceParameters.get("thinking")/10);
+      float unsureInput = (float) (faceParameters.get("unsure")/10);
       timeStamp = (System.currentTimeMillis() - initialTime) * .001;
-      createAndNotify(timeStamp, Math.random());
+
+
+      createAndNotify(timeStamp, agree, disagree, frustrate, concentrate, thinking, unsure);
       try {
         Thread.sleep(1000);
       } catch (InterruptedException ex) {
       }
+
     }
   }
 
-  private void createAndNotify(double timestampsystem, double s) {
-                  System.out.println("notifying ...");
 
-    data = new Data(timestampsystem, s);
+  private void createAndNotify(double timestampsystem, float agreement, float disagreement, float frustrate, float concentrating, float thinking, float unsure) {
+    System.out.println("notifying ...");
+
+    data = new Data(timestampsystem, agreement, concentrating, disagreement, frustrate, thinking, unsure);
+    data.setData( timestampsystem, agreement, concentrating, disagreement,frustrate, thinking, unsure);
+
+    System.out.println(data.toString());
     setChanged();
     notifyObservers();
   }
-
 }
