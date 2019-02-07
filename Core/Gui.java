dbf8c0ae22b837a1f9eab54ad5e3eeecad6865b7
bhavanavenkat <33141616+bhavanavenkat@users.noreply.github.com>
2019-02-06 17:08:45 -0700

    added pupils' size, validation, fixation, aoi and refactored

--- Core/Gui.java
+++ Core/Gui.java
@@ -8,6 +8,9 @@ import java.awt.*;
 import java.util.Calendar;
 import java.awt.event.*;
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**This runs the GUI simulator for Eye Tracking Device
  *  The simulator shows movement of eyes and validation scroller value from 0 - 4
@@ -16,29 +19,28 @@ import java.text.DecimalFormat;
 public class Gui extends JPanel implements ActionListener {
 
   private static Model model;
-
   private final int PORT = 1594;
   protected JLabel labelPublishPort;
   private final JButton buttonConnect = new JButton("run");
 
   private Scale mousePosition = new Scale(0, 0);
-  private int scroll;
-  private float rscroll;
-  private float lscroll;
+  private int validation;
+  private long pupily;
+  private long pupilx;
+  static long timeStamp = 0;
+  private long fixation;
+  private long aoi=0;
   static Data data;
-  //private Scroll window = new Scroll();
   private Eye leftEye = new Eye(350 - 120, 300, 100, 20);
   private Eye rightEye = new Eye(350 + 120, 300, 100, 20);
-
   private Font font = new Font("Arial", Font.PLAIN, 50);
   private Font font2 = new Font("Arial", Font.PLAIN, 150);
-  static JSlider slider_1 = new JSlider();
-  //static JSlider slider_2 = new JSlider();
   final DecimalFormat df = new DecimalFormat("0.####");
+  static JSlider Validation = new JSlider();
   final static JTextField text = new JTextField(50);
-  final static DoubleJSlider slider_2 = new DoubleJSlider(0, 5000, 0, 1000);
-  final static DoubleJSlider slider_3 = new DoubleJSlider(0, 5000, 0, 1000);
-
+  final static pupilControl Pupilx = new pupilControl(0, 5000, 0, 1000);
+  final static pupilControl Pupily = new pupilControl(0, 5000, 0, 1000);
+  HashMap<Set<Integer>,Long> grid = new HashMap<>();
   private Component createPanelSouth() {
 
     JPanel labels = new JPanel();
@@ -62,33 +64,33 @@ public class Gui extends JPanel implements ActionListener {
     setPreferredSize(new Dimension(800, 600));
     addMouseMotionListener(new MouseHandler());
     this.add(createPanelSouth(), BorderLayout.SOUTH);
-    slider_1.setMaximum(4);
-    slider_1.setMajorTickSpacing(1);
-    slider_1.setPaintTicks(true);
-    slider_1.setValue(0);
-    slider_1.setFont(new Font("Calibri", Font.BOLD, 16));
-    slider_1.setToolTipText("Validating");
-    slider_1.setForeground(Color.WHITE);
-    slider_1.setBackground(Color.PINK);
-    slider_1.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
-    slider_1.setPaintLabels(true);
-    slider_1.addChangeListener(new ChangeListener(){ public void stateChanged(ChangeEvent e) {
-      scroll = slider_1.getValue(); }});
+    Validation.setMaximum(4);
+    Validation.setMajorTickSpacing(1);
+    Validation.setPaintTicks(true);
+    Validation.setValue(0);
+    Validation.setFont(new Font("Calibri", Font.BOLD, 16));
+    Validation.setToolTipText("Validating");
+    Validation.setForeground(Color.WHITE);
+    Validation.setBackground(Color.PINK);
+    Validation.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+    Validation.setPaintLabels(true);
+    Validation.addChangeListener(new ChangeListener(){ public void stateChanged(ChangeEvent e) {
+        validation = Validation.getValue(); }});
   
-    slider_2.setFont(new Font("Calibri", Font.BOLD, 20));
-    slider_2.setToolTipText("PUPIL RIGHT");
-    slider_2.setForeground(Color.WHITE);
-    slider_2.setBackground(Color.GRAY);
-    slider_2.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
-    slider_2.setPaintLabels(true);
-    slider_2.setPaintTicks(true);
-    slider_2.setPreferredSize(new Dimension(150,150));
-    slider_2.addChangeListener(new ChangeListener(){
+    Pupilx.setFont(new Font("Calibri", Font.BOLD, 20));
+    Pupilx.setToolTipText("PUPIL RIGHT");
+    Pupilx.setForeground(Color.WHITE);
+    Pupilx.setBackground(Color.GRAY);
+    Pupilx.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+    Pupilx.setPaintLabels(true);
+    Pupilx.setPaintTicks(true);
+    Pupilx.setPreferredSize(new Dimension(150,150));
+    Pupilx.addChangeListener(new ChangeListener(){
         @Override
         public void stateChanged(ChangeEvent e) {
-            text.setText(df.format(slider_2.getScaledValue()));
-            rscroll = (float) slider_2.getScaledValue();
-            rightEye.setR2(rscroll*15);
+            text.setText(df.format(Pupilx.getScaledValue()));
+            pupily = (long) Pupilx.getScaledValue();
+            rightEye.setR2(pupily*15);
             repaint();
         }
     });
@@ -96,29 +98,29 @@ public class Gui extends JPanel implements ActionListener {
         @Override
         public void keyReleased(KeyEvent ke) {
             String typed = text.getText();
-            slider_2.setValue(0);
+            Pupilx.setValue(0);
             if(!typed.matches("\\d+(\\.\\d*)?")) {
                 return;
             }
-            double value = Double.parseDouble(typed)*slider_2.scale;
-            slider_2.setValue((int)value);
+            double value = Double.parseDouble(typed)*Pupilx.scale;
+            Pupilx.setValue((int)value);
         }
     });
     
-    slider_3.setFont(new Font("Calibri", Font.BOLD, 16));
-    slider_3.setToolTipText("PUPIL LEFT");
-    slider_3.setForeground(Color.WHITE);
-    slider_3.setBackground(Color.GRAY);
-    slider_3.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
-    slider_3.setPaintLabels(true);
-    slider_3.setPaintTicks(true);
-    slider_3.setPreferredSize(new Dimension(150,150));
-    slider_3.addChangeListener(new ChangeListener(){
+    Pupily.setFont(new Font("Calibri", Font.BOLD, 16));
+    Pupily.setToolTipText("PUPIL LEFT");
+    Pupily.setForeground(Color.WHITE);
+    Pupily.setBackground(Color.GRAY);
+    Pupily.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+    Pupily.setPaintLabels(true);
+    Pupily.setPaintTicks(true);
+    Pupily.setPreferredSize(new Dimension(150,150));
+    Pupily.addChangeListener(new ChangeListener(){
         @Override
         public void stateChanged(ChangeEvent e) {
-            text.setText(df.format(slider_3.getScaledValue()));
-            lscroll = (float) slider_3.getScaledValue();
-            leftEye.setR2(lscroll*15);
+            text.setText(df.format(Pupily.getScaledValue()));
+            pupilx = (long) Pupily.getScaledValue();
+            leftEye.setR2(pupilx*15);
             repaint();
         }
     });
@@ -126,17 +128,16 @@ public class Gui extends JPanel implements ActionListener {
         @Override
         public void keyReleased(KeyEvent ke) {
             String typed = text.getText();
-            slider_3.setValue(0);
+            Pupily.setValue(0);
             if(!typed.matches("\\d+(\\.\\d*)?")) {
                 return;
             }
-            double value = Double.parseDouble(typed)*slider_3.scale;
-            slider_3.setValue((int)value);
+            double value = Double.parseDouble(typed)*Pupily.scale;
+            Pupily.setValue((int)value);
         }
     });
     
   }
-
 
   @Override
   public void paintComponent(Graphics g) {
@@ -144,17 +145,11 @@ public class Gui extends JPanel implements ActionListener {
 
     leftEye.draw(g, mousePosition);
     rightEye.draw(g, mousePosition);
-
-	        /*g.setFont(font);
-	        g.drawString("Eye Tracking Simulator", 100, 180);
-	        /*g.setFont(font2);
-	        g.drawString("+1", 180, 350);
-	        g.setFont(font);
-	        g.drawString("subscribers :) !!!", 220, 460);*/
   }
 
 
   private class MouseHandler extends MouseAdapter {
+
     @Override
     public void mouseMoved(MouseEvent e) {
       mousePosition.setX(e.getX());
@@ -164,11 +159,33 @@ public class Gui extends JPanel implements ActionListener {
       calendar.set(Calendar.MINUTE, 0);
       calendar.set(Calendar.SECOND, 0);
       long initialTime = calendar.getTimeInMillis();
-      double timeStamp = 0;
-      timeStamp = (System.currentTimeMillis() - initialTime) * .001;
-      data=new Data(timeStamp,e.getX(),e.getY(),scroll);
+      long currentTimeStamp = (long)((System.currentTimeMillis() - initialTime) * .001);
+      fixation= currentTimeStamp-timeStamp;
+      timeStamp = (long)((System.currentTimeMillis() - initialTime) * .001);
+      aoi=calculateAoi(e.getX(),e.getY(),fixation);
+      data=new Data(timeStamp,e.getX(),e.getY(),pupily,pupilx,validation,fixation,aoi);
       repaint();
     }
+
+      public long calculateAoi(int x,int y,long fixation){
+        long value;
+        long max=0;
+        Set<Integer> pixel=new HashSet<>();
+          pixel.add(x);
+          pixel.add(y);
+          if(grid.containsKey(pixel)){
+              value=grid.get(pixel)+fixation;
+              if(value>max)
+                  max=value;
+              grid.put(pixel,value);
+          }
+          else{
+              grid.put(pixel,fixation);
+              if(fixation>max)
+                  max=fixation;
+          }
+          return max;
+      }
   }
 
   @Override
@@ -206,10 +223,10 @@ public class Gui extends JPanel implements ActionListener {
         frame.setResizable(false);
         frame.setBounds(200, 200, 1000, 700);
         frame.setBackground(Color.YELLOW);
-        frame.getContentPane().add(slider_1, BorderLayout.SOUTH);
+        frame.getContentPane().add(Validation, BorderLayout.SOUTH);
         frame.getContentPane().add(text, BorderLayout.NORTH);
-        frame.getContentPane().add(slider_2, BorderLayout.EAST);
-        frame.getContentPane().add(slider_3, BorderLayout.WEST);
+        frame.getContentPane().add(Pupilx, BorderLayout.EAST);
+        frame.getContentPane().add(Pupily, BorderLayout.WEST);
      
         gui.requestFocus();
       }
@@ -218,11 +235,11 @@ public class Gui extends JPanel implements ActionListener {
   }
 }
 
-class DoubleJSlider extends JSlider {
+class pupilControl extends JSlider {
 
     final int scale;
 
-    public DoubleJSlider(int min, int max, int value, int scale) {
+    public pupilControl(int min, int max, int value, int scale) {
         super(min, max, value);
         this.scale = scale;
     }
