723b4d81039e98f64ca4c6fbcc98c0741d8b6151
saran1856 <skandim2@asu.edu>
2019-02-12 17:34:11 -0700

    Revert "fix: fixed ExpressionController to send values only once over radio"
    This reverts commit 0fd8e08b81e7e9e2d51329e5c7b44323c3001bdd.
    
    revert: reverted last commit due to some issues.

--- controller/ExpressionController.java
+++ controller/ExpressionController.java
@@ -1,7 +1,5 @@
 package controller;
 
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.util.Enumeration;
 
 import javax.swing.AbstractButton;
@@ -10,52 +8,59 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import Core.Data;
+import Core.DataGenerator;
+import Model.ExpressionData;
+
 public class ExpressionController {
-	
+
 	private JSlider smirkSlider;
 	private JSlider clenchSlider;
 	private JSlider laughSlider;
 	private ButtonGroup blinkButtonGroup;
 	private ButtonGroup visionButtonGroup;
 	private ButtonGroup eyeBrowButtonGroup;
-	
 	private ExpressionData expressionData;
-	
+	private DataGenerator dg ;
+
+	private double blink, smirkLeft, smirkRight, lookLeft, lookRight, smile, clench, laugh, winkLeft, winkRight, raiseBrow, furrow = 0;
+
 	public ExpressionController(JSlider smirkSlider, JSlider clenchSlider, JSlider laughSlider,
-				ButtonGroup blinkButtonGroup, ButtonGroup visionButtonGroup, ButtonGroup eyeBrowButtonGroup) {
-		
+			ButtonGroup blinkButtonGroup, ButtonGroup visionButtonGroup, ButtonGroup eyeBrowButtonGroup, DataGenerator dg) {
+
 		this.smirkSlider = smirkSlider;
 		this.clenchSlider = clenchSlider;
 		this.laughSlider = laughSlider;
 		this.blinkButtonGroup = blinkButtonGroup;
 		this.visionButtonGroup = visionButtonGroup;
 		this.eyeBrowButtonGroup = eyeBrowButtonGroup;
-		
-		expressionData = new ExpressionData();
-		
+		this.dg = dg;
+
+
+
 		smirkSlider.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent event) {
 				updateExpressionModel();
 			}
 		});
-		
+
 		clenchSlider.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent event) {
 				updateExpressionModel();
 			}
 		});
-		
+
 		laughSlider.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent event) {
 				updateExpressionModel();
 			}
 		});
-		
+
 		setChangeListeners(blinkButtonGroup);
 		setChangeListeners(visionButtonGroup);
 		setChangeListeners(eyeBrowButtonGroup);
 	}
-	
+
 	/*
 	 * method to set change listeners for each button in a button group.
 	 * @param: buttonGroup of type ButtonGroup
@@ -63,17 +68,16 @@ public class ExpressionController {
 	 */
 	private void setChangeListeners(ButtonGroup buttonGroup) {
 		for(Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
-			buttons.nextElement().addItemListener((new ItemListener() {
-				public void itemStateChanged(ItemEvent event) {
-					if(event.getStateChange() == ItemEvent.SELECTED)
-						updateExpressionModel();
+			buttons.nextElement().addChangeListener((new ChangeListener() {
+				public void stateChanged(ChangeEvent event) {
+					updateExpressionModel();
 				}
 			}));
 		}
 	}
-	
+
 	/*
-	 * method to get selected option for each button in a button group.
+	 * method to set change listeners for each button in a button group.
 	 * @param: buttonGroup of type ButtonGroup
 	 * referred from: https://stackoverflow.com/questions/201287/
 	 */
@@ -86,21 +90,109 @@ public class ExpressionController {
 		}
 		return "None";
 	}
-	
+
 	private double getTickDecimal(int tickValue) {
 		return (double)tickValue/100;
 	}
-	
+
 	private void updateExpressionModel() {
+		expressionData = new ExpressionData();
+
 		double smirkSliderValue = getTickDecimal(this.smirkSlider.getValue());
 		double clenchSliderValue = getTickDecimal(this.clenchSlider.getValue());
-		double laughSliderValue = getTickDecimal(this.laughSlider.getValue());
+		laugh = getTickDecimal(this.laughSlider.getValue());
 		String blinkOption = getSelectedOption(this.blinkButtonGroup);
 		String visionOption = getSelectedOption(this.visionButtonGroup);
 		String eyeBrowOption = getSelectedOption(this.eyeBrowButtonGroup);
-		
-		expressionData.updateExpressionData(blinkOption, visionOption, eyeBrowOption, 
-											smirkSliderValue, clenchSliderValue, laughSliderValue);
+		Data data= (Data)dg.getObject();
+
+		if (smirkSliderValue < 0) {
+			smirkLeft = Math.abs(smirkSliderValue);
+			smirkRight = 0.0;
+		}
+		else {
+			smirkLeft = 0.0;
+			smirkRight = Math.abs(smirkSliderValue);
+		}
+
+		if (clenchSliderValue < 0) {
+			clench = Math.abs(clenchSliderValue);
+			smile = 0.0;
+		}
+		else {
+			clench = 0.0;
+			smile = Math.abs(clenchSliderValue);
+		}
+
+		switch(blinkOption) {
+		case "None" :{
+			blink = 0;
+			winkLeft = 0;
+			winkRight = 0;
+			break;
+
+		}
+		case "Blink" :{
+			blink = 1;
+			winkLeft = 0;
+			winkRight = 0;
+			break;
+
+		}
+		case "Wink Left" :{
+			blink = 0;
+			winkLeft = 1;
+			winkRight = 0;
+			break;
+
+		}
+		case "Wink Right" :{
+			blink = 0;
+			winkLeft = 0;
+			winkRight = 1;
+
+		}
+		}
+
+		switch(visionOption) {
+		case "None" :{
+			lookLeft = 0;
+			lookRight = 0;
+			break;
+		}
+
+
+		case "Look Left" :{
+			lookLeft = 1;
+			lookRight = 0;
+			break;
+		}
+		case "Look Right" :{
+			lookLeft = 0;
+			lookRight = 1;
+		}
+		}
+
+		switch(eyeBrowOption) {
+		case "None" :{
+			raiseBrow = 0;
+			furrow = 0;
+			break;
+		}
+		case "Rasie Brow" :{
+			raiseBrow = 1;
+			furrow = 0;
+			break;
+		}
+		case "Furrow" :{
+			raiseBrow = 0;
+			furrow = 1;
+		}
+		}
+
+		System.out.println(blink+" "+ winkLeft+" "+winkRight+" "+lookLeft+" "+lookRight+" "+smirkLeft+" "+smirkRight+" "+clench+" "+smile+" "+laugh+" "+raiseBrow+" "+furrow);
+		expressionData.updateExpressionData(blink,winkLeft,winkRight,lookLeft,lookRight,smirkLeft,smirkRight,clench,smile,laugh,raiseBrow,furrow);
+		data.setExpressive(expressionData);
 	}
 
 }
